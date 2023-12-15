bfca12129ab51d671678a326c2f29030585f9566
SanerYesil <saneryesil@hotmail.com>
2023-12-15 19:01:50 +0300

    home page designed

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -9,68 +9,80 @@ class homePage extends StatelessWidget {
       body: Container(
         alignment: Alignment.center,
         decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('assets/images/homePageBackground.jpg'),
-                fit: BoxFit.fill)),
+          image: DecorationImage(
+              image: AssetImage('assets/images/homePageBackground.jpg'),
+              fit: BoxFit.cover),
+        ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text(
-              'Günlüğüne hoşgeldin \n User',
-              style: TextStyle(
-                fontSize: 32,
-                color: Colors.white,
+            Padding(
+              padding: const EdgeInsets.only(top: 200),
+              child: Text(
+                'Günlüğüne hoşgeldin \n User',
+                style: TextStyle(
+                  fontSize: 32,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
               ),
-              textAlign: TextAlign.center,
             ),
-            Text(
-              'Bugün neler yaşadın \n Hemen anılarını kaydet!',
-              style: TextStyle(
-                fontSize: 32,
-                color: Colors.white,
+            Padding(
+              padding: const EdgeInsets.only(top: 300),
+              child: Text(
+                'Bugün neler yaşadın? \n Hemen anılarını kaydet!',
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
               ),
-              textAlign: TextAlign.center,
             ),
             Row(
-
               // crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-
-                ButtonBar(
-
-                  children: [
-                    IconButton(
-                      constraints: BoxConstraints(
-                        maxHeight: 50,
-
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 0),
+                  child: ButtonBar(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 50),
+                        child: IconButton(
+                            constraints: BoxConstraints(),
+                            padding: EdgeInsets.all(20),
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.calendar_month,
+                              size: 64,
+                              color: Colors.white,
+                            )),
                       ),
-                      padding: EdgeInsets.all(20),
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.calendar_month,
-                          size: 64,
-                          color: Colors.white,
-                        )),
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.add_circle_outline,
-                        size: 96,
-                        color: Colors.white,
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 50),
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.add_circle_outline,
+                            size: 100,
+                            color: Colors.white,
+                          ),
+                        ),
                       ),
-                      alignment: Alignment.center,
-                      style: ButtonStyle(),
-                    ),
-                    IconButton(
-                      padding: EdgeInsets.all(20),
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.account_circle,
-                          size: 64,
-                          color: Colors.white,
-                        ))
-                  ],
+                      Padding(
+                        padding: const EdgeInsets.only(top: 50),
+                        child: IconButton(
+                          padding: EdgeInsets.all(20),
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.account_circle,
+                            size: 64,
+                            color: Colors.white,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 )
               ],
             )
